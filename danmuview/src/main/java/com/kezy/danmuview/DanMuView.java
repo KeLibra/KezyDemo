5d646303825d3f35cd2a2cca52b6e5cd756ef16a
Kezy <zhongyang.ke@ximalaya.com>
2022-05-16 22:48:39 +0800

    [feat] 弹幕进阶版

--- danmuview/src/main/java/com/kezy/danmuview/DanMuView.java
+++ danmuview/src/main/java/com/kezy/danmuview/DanMuView.java
@@ -1,6 +1,7 @@
 package com.kezy.danmuview;
 
 import android.animation.Animator;
+import android.animation.AnimatorListenerAdapter;
 import android.animation.LayoutTransition;
 import android.animation.ObjectAnimator;
 import android.animation.PropertyValuesHolder;
@@ -13,6 +14,8 @@ import android.os.Handler;
 import android.os.Message;
 import android.text.TextUtils;
 import android.util.AttributeSet;
+import android.util.Log;
+import android.util.Printer;
 import android.view.Gravity;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -30,16 +33,20 @@ import androidx.core.util.Pools;
  */
 public class DanMuView extends LinearLayout {
 
+    private boolean isPlaying = false;
     private LayoutTransition transition;
 
+    private final int DEFAULT_MAX_ITEM = 4;
+    private final int DEFAULT_DELAY_TIME = 1 * 1000;
     //最大展示数量 默认四条
-    private int maxItem = 4;
+    private int maxItem = DEFAULT_MAX_ITEM;
+    private int delayTime = DEFAULT_DELAY_TIME;
 
     private String[] texts = new String[]{
-            "火来我在灰烬中等你",
-            "我对这个世界没什么可说的。我对这个世界没什么可说的。我对这个世界没什么可说的。",
-            "侠之大者，为国为民。",
-            "为往圣而继绝学"};
+            "1. 火来我在灰烬中等你",
+            "2. 我对这个世界没什么可说的。我对这个世界没什么可说的。我对这个世界没什么可说的。",
+            "3. 侠之大者，为国为民。",
+            "4. 为往圣而继绝学"};
 
     public DanMuView(Context context) {
         super(context);
@@ -57,39 +64,37 @@ public class DanMuView extends LinearLayout {
         super(context, attrs, defStyleAttr);
     }
 
+    public boolean isPlaying() {
+        return isPlaying;
+    }
+
     private void init() {
         setShowDividers(SHOW_DIVIDER_MIDDLE);
+        setOrientation(VERTICAL);
+
         transition = new LayoutTransition();
         //添加动画
         ObjectAnimator valueAnimator = ObjectAnimator.ofFloat(null, "alpha", 0, 1);
-        valueAnimator.addListener(new Animator.AnimatorListener() {
+        valueAnimator.addListener(new AnimatorListenerAdapter() {
             @Override
             public void onAnimationStart(Animator animation) {
+                super.onAnimationStart(animation);
                 //当前展示超过四条，执行删除动画
-                if (getChildCount() == 4) {
+                if (getChildCount() >= maxItem) {
                     handler.sendEmptyMessage(1);
                 }
             }
 
             @Override
             public void onAnimationEnd(Animator animation) {
-                if (getChildCount() == 5)
+                super.onAnimationEnd(animation);
+                if (getChildCount() > maxItem) {
                     //动画执行完毕，删除view
                     handler.sendEmptyMessage(2);
-            }
-
-            @Override
-            public void onAnimationCancel(Animator animation) {
-
-            }
-
-            @Override
-            public void onAnimationRepeat(Animator animation) {
-
+                }
             }
         });
         transition.setAnimator(LayoutTransition.APPEARING, valueAnimator);
-        valueAnimator.setDuration(300);
         //删除动画
         PropertyValuesHolder alpha = PropertyValuesHolder.ofFloat("alpha", 0, 0);
         ObjectAnimator objectAnimator = ObjectAnimator.ofPropertyValuesHolder(null, new PropertyValuesHolder[]{alpha}).setDuration(transition.getDuration(LayoutTransition.DISAPPEARING));
@@ -107,21 +112,11 @@ public class DanMuView extends LinearLayout {
         this.maxItem = maxItem;
     }
 
-    public void setData(String name, String cause) {
-        View view = getTextView();
-        //获取学生姓名
-        TextView nameTxt = view.findViewById(R.id.studentNameTxt);
-        //扣分原因
-        TextView causeTxt = view.findViewById(R.id.causeTxt);
-
-        //设置学生姓名
-        nameTxt.setText(TextUtils.isEmpty(name) ? "佚名" : name);
-        //扣分原因
-        causeTxt.setText(TextUtils.isEmpty(cause) ? "无" : cause);
-        setOrientation(VERTICAL);
-        //添加子布局
-        addView(view);
+    public void setDelayTime(int delayTime) {
+        this.delayTime = delayTime;
     }
+
+    Pools.SimplePool<View> viewSimplePool = new Pools.SimplePool<>(texts.length);
 
     /**
      * 获取子控件布局
@@ -129,13 +124,22 @@ public class DanMuView extends LinearLayout {
      * @return View
      */
     private View getTextView() {
-        return LayoutInflater.from(getContext()).inflate(R.layout.item_live_danmu, null);
+
+        View inView = viewSimplePool.acquire();
+        if (inView == null) {
+            inView = LayoutInflater.from(getContext()).inflate(R.layout.item_live_danmu, null);
+            TextView textView = inView.findViewById(R.id.causeTxt);
+            textView.setText(texts[index]);
+        }
+
+        return inView;
     }
 
     /**
      * 回收
      */
     public void destory() {
+        isPlaying = false;
         if (handler != null) {
             handler.removeCallbacksAndMessages(null);
             handler = null;
@@ -155,20 +159,21 @@ public class DanMuView extends LinearLayout {
             super.handleMessage(msg);
             switch (msg.what) {
                 case 0:
-                    TextView textView = obtainTextView();
-                    addView(textView);
-                    sendEmptyMessageDelayed(0, 800);
-                    setOrientation(VERTICAL);
-                    //添加子布局
-
-                    index++;
-                    if (index == 4) {
+                    if (index >= texts.length) {
                         index = 0;
                     }
+                    View textView = getTextView();
+                    addView(textView);
+                    sendEmptyMessageDelayed(0, delayTime);
+                    index++;
                     break;
                 case 1:
-                    //给展示的第一个view增加渐变透明动画
-                    getChildAt(0).animate().alpha(0).setDuration(transition.getDuration(LayoutTransition.APPEARING)).start();
+                    if (getChildAt(0) != null && getChildAt(0).animate() != null
+                            && getChildAt(0).animate().alpha(0) != null
+                            && getChildAt(0).animate().alpha(0).setDuration(transition.getDuration(LayoutTransition.APPEARING)) != null) {
+                        //给展示的第一个view增加渐变透明动画
+                        getChildAt(0).animate().alpha(0).setDuration(transition.getDuration(LayoutTransition.APPEARING)).start();
+                    }
                     break;
                 case 2:
                     //删除顶部view
@@ -178,58 +183,35 @@ public class DanMuView extends LinearLayout {
         }
     };
 
-    Pools.SimplePool<TextView> textViewPool = new Pools.SimplePool<>(texts.length);
-
-    private TextView obtainTextView() {
-        TextView textView = textViewPool.acquire();
-        if (textView == null) {
-            textView = new TextView(getContext());
-            textView.setLayoutParams(new LinearLayout.LayoutParams(LinearLayout.LayoutParams.WRAP_CONTENT, LinearLayout.LayoutParams.WRAP_CONTENT));
-            textView.setPadding(dp2px(10), dp2px(5), dp2px(10), dp2px(5));
-            textView.setTextColor(0xffffffff);
-            textView.setMaxLines(1);
-            textView.setEllipsize(TextUtils.TruncateAt.END);
-            textView.setGravity(Gravity.CENTER);
-            textView.setTextSize(14);
-            textView.setTextColor(Color.parseColor("#fd5353"));
-            Drawable drawable = getResources().getDrawable(R.mipmap.ic_launcher);
-            drawable.setBounds(0, 0, 80, 80);
-            textView.setCompoundDrawablesRelative(drawable, null, null, null);
-            textView.setCompoundDrawablePadding(10);
-            switch (index) {
-                case 0:
-//                    textView.setBackgroundDrawable(ContextCompat.getDrawable(MainActivity.this, R.drawable.ic_launcher_background));
-                    break;
-                case 1:
-//                    textView.setBackgroundDrawable(ContextCompat.getDrawable(MainActivity.this, R.drawable.ic_launcher_background));
-
-                    break;
-                case 2:
-//                    textView.setBackgroundDrawable(ContextCompat.getDrawable(MainActivity.this, R.drawable.ic_launcher_background));
-
-                    break;
-                case 3:
-//                    textView.setBackgroundDrawable(ContextCompat.getDrawable(MainActivity.this, R.drawable.ic_launcher_background));
-
-                    break;
-            }
-        }
-        textView.setText(texts[index]);
-        return textView;
-    }
-
-    private int dp2px(float dipValue) {
-        final float scale = getContext().getResources().getDisplayMetrics().density;
-        return (int) (dipValue * scale + 0.5F);
-    }
 
     public void startPlay() {
-        handler.sendEmptyMessage(0);
+        if (isPlaying) {
+            return;
+        }
+        if (handler != null) {
+            isPlaying = true;
+            handler.sendEmptyMessage(0);
+        }
     }
 
-    public void stopPlay () {
-        handler.removeMessages(0);
+    public void stopPlay() {
+        if (handler != null) {
+            isPlaying = false;
+            handler.removeMessages(0);
+        }
     }
 
+    @Override
+    protected void onAttachedToWindow() {
+        super.onAttachedToWindow();
+        startPlay();
+        Log.e("-------msg", " ------ onAttachedToWindow -----");
+    }
 
+    @Override
+    protected void onDetachedFromWindow() {
+        super.onDetachedFromWindow();
+        stopPlay();
+        Log.e("-------msg", " ------ onDetachedFromWindow -----");
+    }
 }
--- danmuview/src/main/java/com/kezy/danmuview/MainActivity.java
+++ danmuview/src/main/java/com/kezy/danmuview/MainActivity.java
@@ -3,19 +3,34 @@ package com.kezy.danmuview;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
 
     private DanMuView danMuView;
+    private Button btn;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        btn = findViewById(R.id.btn_click);
         danMuView = findViewById(R.id.danmu_view);
+        danMuView.setMaxItem(2);
+        danMuView.setDelayTime(2500);
 
-        danMuView.startPlay();
+        btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (danMuView.isPlaying()) {
+                    danMuView.stopPlay();
+                } else {
+                    danMuView.startPlay();
+                }
+            }
+        });
     }
 }
