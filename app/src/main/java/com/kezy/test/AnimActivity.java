7aa2c276f57ecdf3d836ecdb30f40ce4780f0ec0
Kezy <zhongyang.ke@ximalaya.com>
2023-08-25 20:23:02 +0800

    提交test

--- app/src/main/java/com/kezy/test/AnimActivity.java
+++ app/src/main/java/com/kezy/test/AnimActivity.java
@@ -19,6 +19,8 @@ public class AnimActivity extends AppCompatActivity {
     TextView tv_btn, tv_bg;
     Button bbbbb, aaaaa;
 
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
